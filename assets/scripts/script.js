73346d6732bc57754bc4f6069c94da153cfa28fe
Gabriel Morais <gabriel.m.g.morais@gmail.com>
2022-06-25 16:56:59 -0300

    adicionado event listener alternado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,43 +1,51 @@
+
+document.getElementById("adc").addEventListener("click", createSpam)
+
+
 function createSpam(){
 
     //variaveis globais (elementos)
     var divSpan = document.createElement("div")
     var nomelabel = document.createElement("label")
     var nomeinput = document.createElement("input")
-    var button = document.createElement("button")
-    var buttonText = document.createTextNode("criar")
     
     //variaveis globais (referencias)
-    var divReferencia = document.getElementsByClassName("conteiner")[0]
-    var nomeItem = document.getElementById("nomeItem")
+    var divReferencia = document.getElementById("conteiner")
 
     //variaveis globais (atributos)
     divSpan.setAttribute("class", "span")
+    divSpan.setAttribute("id", "span")
     nomeinput.setAttribute("id", "nomeItem")
-    button.setAttribute("id", "submit")
-    button.setAttribute("onclick", "addTask(nomeItem.value)")
     nomelabel.innerHTML = "Nome da tarefa:"
-
 
     //Define hierarquia dos novos elementos
     divReferencia.appendChild(divSpan)
     divSpan.appendChild(nomelabel);
     divSpan.appendChild(nomeinput);
-    divSpan.appendChild(button)
-    button.appendChild(buttonText)
+
+    document.getElementById("adc").removeEventListener("click", createSpam)
+    document.getElementById("adc").addEventListener("click", createTask)
 }
 
-function addTask(nomeItem){
+function deleteSpam(){ 
+    document.getElementById("span").remove()
+    document.getElementById("adc").removeEventListener("click", createTask)
+    document.getElementById("adc").addEventListener("click", createSpam)
+}
+
+function createTask(){
+
+    var nomeItem = document.getElementById("nomeItem")
 
     //variaveis globais (elementos)
     var novoDiv = document.createElement("div")
     var novoInput = document.createElement("input")
     var novoLabel = document.createElement("label")
+    var labelTextNode = document.createTextNode(nomeItem.value)
 
     //variaveis globais (atributos)
     novoDiv.setAttribute("class", "tasks")
     novoInput.setAttribute("type", "checkbox")
-    novoLabel.innerHTML = nomeItem
 
     //variaveis globais (referencias)
     var divReferencia = document.getElementsByClassName("conteiner")[0]
@@ -46,4 +54,5 @@ function addTask(nomeItem){
     divReferencia.appendChild(novoDiv)
     novoDiv.appendChild(novoInput)
     novoDiv.appendChild(novoLabel)
+    novoLabel.appendChild(labelTextNode)
 }
