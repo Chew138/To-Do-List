4bc7feb7093948da0e7cb6ee29712e3aa6942c0a
Gabriel Morais <gabriel.m.g.morais@gmail.com>
2022-08-16 18:10:29 -0300

    abstracao funcao createElement

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,58 +1,141 @@
 
-document.getElementById("adc").addEventListener("click", createSpam)
+var buttonEl = document.getElementById("adc").addEventListener("click", main)
 
-
-function createSpam(){
-
-    //variaveis globais (elementos)
-    var divSpan = document.createElement("div")
-    var nomelabel = document.createElement("label")
-    var nomeinput = document.createElement("input")
+const element = {
     
-    //variaveis globais (referencias)
-    var divReferencia = document.getElementById("conteiner")
-
-    //variaveis globais (atributos)
-    divSpan.setAttribute("class", "span")
-    divSpan.setAttribute("id", "span")
-    nomeinput.setAttribute("id", "nomeItem")
-    nomelabel.innerHTML = "Nome da tarefa:"
-
-    //Define hierarquia dos novos elementos
-    divReferencia.appendChild(divSpan)
-    divSpan.appendChild(nomelabel);
-    divSpan.appendChild(nomeinput);
-
-    document.getElementById("adc").removeEventListener("click", createSpam)
-    document.getElementById("adc").addEventListener("click", createTask)
+    tag: "",
+    class: "",
+    type: "",
+    text: "",
+    id: "",
+    eventListener: ""
+    
 }
 
-function deleteSpam(){ 
-    document.getElementById("span").remove()
-    document.getElementById("adc").removeEventListener("click", createTask)
-    document.getElementById("adc").addEventListener("click", createSpam)
+function createElement(element, reference){
+    
+    try{
+        if( element.tag.length === 0){
+            throw console.log("Admin message: Nenhuma tag HTML foi definida \n")
+        }
+
+        let newElement = document.createElement(element.tag)
+
+        if(element.class.length != 0)
+        {
+            newElement.setAttribute("class", element.class)
+        }
+        
+        if(element.id.length != 0){
+            newElement.setAttribute("id", element.id)
+        }
+        
+        if(element.type.length != 0){
+            newElement.setAttribute("type", element.type)
+        }
+        
+        if(element.text.length != 0){
+            newElement.innerHTML = element.text
+        }
+
+        reference.appendChild(newElement)
+    }
+    catch(e){
+        console.log(e)
+    }
 }
 
-function createTask(){
+function main(){
 
-    var nomeItem = document.getElementById("nomeItem")
+    let spammerConteinerEl = document.getElementById("spammer")
+    let taskConteinerEl = document.getElementById("conteiner")
 
-    //variaveis globais (elementos)
-    var novoDiv = document.createElement("div")
-    var novoInput = document.createElement("input")
-    var novoLabel = document.createElement("label")
-    var labelTextNode = document.createTextNode(nomeItem.value)
+    if(spammerConteinerEl.childElementCount == 0){
 
-    //variaveis globais (atributos)
-    novoDiv.setAttribute("class", "tasks")
-    novoInput.setAttribute("type", "checkbox")
+        //gera o spammer
+        element.tag = "span"
+        element.class = "span"
+        element.id = "span"
+        createElement(element , spammerConteinerEl)
+        clearObject()
 
-    //variaveis globais (referencias)
-    var divReferencia = document.getElementsByClassName("conteiner")[0]
+        //texto do spammer
+        let spammerEl = document.getElementById("span")
+        element.tag = "p"
+        element.text = "Nome da tarefa"
+        createElement(element, spammerEl)
+        clearObject()
 
-    //Define hierarquia dos novos elementos
-    divReferencia.appendChild(novoDiv)
-    novoDiv.appendChild(novoInput)
-    novoDiv.appendChild(novoLabel)
-    novoLabel.appendChild(labelTextNode)
+        //input
+        element.tag = "input"
+        element.id = "input"
+        createElement(element, spammerEl)
+        clearObject()
+    }
+    else{
+
+        //conteiner task
+        element.tag = "div"
+        element.class = "tasks"
+        createElement(element, taskConteinerEl)
+        clearObject()
+
+        //checkbox
+        element.tag = "input"
+        element.type = "checkbox"
+        createElement(element, taskConteinerEl.lastChild)
+        clearObject()
+
+        //label
+        element.tag = "label"
+        element.text = document.getElementById("input").value
+        createElement(element, taskConteinerEl.lastChild)
+        clearObject()
+
+        let getLastInputEl = document.getElementsByTagName("input").length-1
+        let inputEl = document.getElementsByTagName("input")[getLastInputEl]
+
+        let getLastLabelEl =  document.getElementsByTagName("label").length-1
+        let labelEl = document.getElementsByTagName("label")[getLastLabelEl]
+
+        let getLastDivEl = document.getElementsByClassName("tasks").length-1
+        let divTaskEl = document.getElementsByClassName("tasks")[getLastDivEl]
+
+        inputEl.addEventListener("click",  () => {
+            if(inputEl.checked){
+
+                //efeito de exclusao do elemento
+                setTimeout( () => {
+                    
+                    labelEl.style.color = "green"
+                    labelEl.classList.toggle("taskToggle")
+
+                    setTimeout(() => {
+                        labelEl.style.color = "red"
+
+                        setTimeout(() => {
+                            
+                            inputEl.remove()
+                            labelEl.remove()
+                            divTaskEl.remove()
+
+                        }, 1500);
+                    }, 1000);
+
+                }, 800)
+
+            }
+            else{
+                labelEl.classList.toggle("taskToggle")
+            }
+        })
+    }
+}
+
+function clearObject(){
+    element.tag = ""
+    element.class = ""
+    element.text = ""
+    element.id = ""
+    element.type = ""
 }
